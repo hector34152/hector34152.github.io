2f7f96c618326b895ad3698a0de4768320862e2e
hector34152 <128176614+hector34152@users.noreply.github.com>
2024-06-04 12:08:31 -0600

    Add files via upload

---
+++ portafolio/TicTacToe/script.js
@@ -0,0 +1,70 @@
+var matriz = [[0,0,0],[0,0,0],[0,0,0]];
+var sumas = [0,0,0,0,0,0,0,0];
+var ganador = "";
+
+function crear(){
+    var div = document.getElementById("div");
+    div.innerHTML = "";
+    div.innerHTML = "<table id = 'tablero'>";
+    limpiar();
+    var tablero = document.getElementById("tablero");
+    for(let i=1;i<4;i++){
+        tablero.innerHTML += "<tr id = '"+i+"'>";
+        for(let j=1;j<4;j++){
+            document.getElementById(i).innerHTML += "<td><button type = 'button' class = 'tic' onclick = 'marca("+i+","+j+");' id = '"+i+","+j+"'></button>";
+        }
+    }
+}function marca(a,b){
+    let ab = a+","+b;
+    let c = document.getElementById(ab);
+    c.innerText = "X";
+    matriz[a-1][b-1]=1;
+    recorrer();
+}
+function recorrer(){
+    let k = 1;
+    sumas[0] = matriz[2][0]+matriz[1][1]+matriz[0][2];
+    for(let i=0;i<3;i++){
+        for(let j=0;j<3;j++){
+            sumas[k] += matriz[i][j];
+        }
+        k++;
+    }
+    for(let i=0;i<3;i++){
+        for(let j=0;j<3;j++){
+            sumas[k] += matriz[j][i];
+        }
+        k++;
+    }
+    sumas[7] = matriz[0][0]+matriz[1][1]+matriz[2][2];
+    if(verificar()){
+
+    }
+    else{
+        console.log(ganador);
+        console.log(matriz);
+        console.log(sumas);
+    }
+}
+function verificar(){
+    for(let i=0;i<sumas.length;i++){
+        if(sumas[i]==3){
+            ganador = "x";
+            return false;
+        }
+        else if(sumas[i]==30){
+            ganador = "o";
+            return false;
+        }
+    }
+}
+function limpiar(){
+    for(let i=0;i<3;i++){
+        for(let j=0;j<3;j++){
+            matriz[i][j]=0;
+        }
+    }
+    for(let i=0;i<sumas.length;i++){
+        sumas[i]=0;
+    }
+}
