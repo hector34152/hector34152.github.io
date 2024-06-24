60c9af3995ce4428e92b9359acb011b14e30273d
hector34152 <128176614+hector34152@users.noreply.github.com>
2024-06-24 14:18:34 -0600

    Update script.js

--- portafolio/TicTacToe/script.js
+++ portafolio/TicTacToe/script.js
@@ -4,7 +4,6 @@ var ganador = "";
 
 function crear(){
     var div = document.getElementById("div");
-    div.innerHTML = "";
     div.innerHTML = "<table id = 'tablero'>";
     limpiar();
     var tablero = document.getElementById("tablero");
@@ -14,15 +13,19 @@ function crear(){
             document.getElementById(i).innerHTML += "<td><button type = 'button' class = 'tic' onclick = 'marca("+i+","+j+");' id = '"+i+","+j+"'></button>";
         }
     }
-}function marca(a,b){
+}
+function marca(a,b){
     let ab = a+","+b;
     let c = document.getElementById(ab);
     c.innerText = "X";
     matriz[a-1][b-1]=1;
     recorrer();
+    marcaO();
 }
 function recorrer(){
     let k = 1;
+    sumas = [0,0,0,0,0,0,0,0];
+
     sumas[0] = matriz[2][0]+matriz[1][1]+matriz[0][2];
     for(let i=0;i<3;i++){
         for(let j=0;j<3;j++){
@@ -38,23 +41,24 @@ function recorrer(){
     }
     sumas[7] = matriz[0][0]+matriz[1][1]+matriz[2][2];
     if(verificar()){
-
+        alert("Felicidades a ganado "+ganador);
+        crear();
     }
     else{
-        console.log(ganador);
         console.log(matriz);
         console.log(sumas);
+
     }
 }
 function verificar(){
     for(let i=0;i<sumas.length;i++){
         if(sumas[i]==3){
             ganador = "x";
-            return false;
+            return true;
         }
         else if(sumas[i]==30){
             ganador = "o";
-            return false;
+            return true;
         }
     }
 }
